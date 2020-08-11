217425f28c03a0648037cbb34a9b3781469747e6
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-08-12 03:53:11 +0530

    Create Defanging an IP Address.py

---
+++ Defanging an IP Address.py
@@ -0,0 +1,11 @@
+#question4
+#question link=https://leetcode.com/problems/defanging-an-ip-address/
+
+---------------------------------------
+
+#solution
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        address=address.replace('.',"[.]")
+        return address       
+        
