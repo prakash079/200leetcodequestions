9ba9f3a26dd4812f8d84bae5513ee4b97a7712c6
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-08-20 04:24:55 +0530

    Create question19_single-number-ii.py

---
+++ question19_single-number-ii.py
@@ -0,0 +1,10 @@
+#question_link=https://leetcode.com/problems/single-number-ii/
+-----------------------------------------------------------------------
+#solution
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        sum_set=sum(set(nums))
+        sum_nums=sum(nums)
+        res=(sum_set)*3-sum_nums
+        return res//2
+        
